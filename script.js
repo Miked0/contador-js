e4edae78cd09170a62b5acd7579e7bb091cff8bb
Michael Douglas <mike.d@outlook.com.br>
2022-07-09 22:54:38 -0300

    Commit Final
    Adicionado funcionalidades finais no site

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+// botões
 var buttonIncrement = document.querySelector(".bttn");
-var count = document.querySelector(".contador");
+var buttonDecrement = document.querySelector("#bttn_decrementar");
+var buttonReset = document.querySelector("#bttn_reset");
 
+
+// contador
+var count = document.querySelector(".contador");
 var numero = 0;
 
-buttonIncrement.addEventListener("click", function (e) {
+
+buttonIncrement.addEventListener("click", () => {
     numero++;
-    count.textContent = numero;
+    setTimeout(() => {
+        count.textContent = numero
+    }, "200");
+
+   
 })
+
+
+buttonDecrement.addEventListener("click", () => {
+    numero--;
+    setTimeout(() => {
+        count.textContent = numero
+    }, "200");
+ 
+})
+
+buttonReset.addEventListener("click", () => {
+    numero = 0;
+    setTimeout(() => {
+        count.textContent = numero
+    }, "200");
+})
+
+
